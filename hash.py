345c06001fb6fee486959be1d4fbf14868fc0483
Mateusz Stanczak <mateusz.stanczak@pgf.com.pl>
2019-11-23 10:20:24 +0100

    add working v1

--- hash.py
+++ hash.py
@@ -3,15 +3,27 @@
 
 import random
 
+import matplotlib.pyplot as plt
+
 
 class Dictionary:
-    data = []
 
-    def key(self, element): return element
+    def __init__(self, size=10):
+        self.data = []
+        self.size = size
+        self.comparison_count = 0
+
+        for i in range(size):
+            self.data.insert(i, [])
+
+    def key(self, element):
+        return element
 
     def find_index(self, k):
-        h = hash(k) % len(self.data)
+        h = hash(k) % self.size
+        self.comparison_count = 0
         for i in range(0, len(self.data[h])):
+            self.comparison_count = self.comparison_count + 1
             if self.key(self.data[h][i]) == k:
                 return h, i
         return h, -1
@@ -25,7 +37,7 @@ class Dictionary:
     def insert(self, element):
         h, i = self.find_index(self.key(element))
         if i == -1:
-            self.data.append(element)
+            self.data[h].append(element)
         else:
             self.data[h][i] = element
 
@@ -35,19 +47,34 @@ class Dictionary:
             self.data[h][i] = self.data[h][-1]
             self.data.remove(self.data[h][-1])
 
+    def get_comparison_count(self):
+        return self.comparison_count
+
+
+def graph(x, y):
+    plt.plot(x, y)
+    plt.xlabel('x - liczba elementow w slowniku')
+    plt.ylabel('y - srednia liczba prownan potrzebna by znalezc element')
+    plt.title('Wykres statystyk')
+    plt.show()
+
 
 def init():
-    dictionary = Dictionary()
+    x = []
+    y = []
 
-    # for n in range(100000):
-    #     dictionary.insert(random.randint(0, 1000000))
-    dictionary.insert(1)
-    dictionary.insert(2)
-    dictionary.insert(3)
+    for i in range(100):
+        dictionary = Dictionary()
+        elements_count = i * 10
+        max_value = 1000
 
-    dictionary.delete(2)
+        for n in range(elements_count + 1):
+            dictionary.insert(random.randint(0, max_value))
 
-    # print(dictionary.find(random.randint(0, 1000000)))
+        x.append(elements_count)
+        y.append(dictionary.get_comparison_count())
+
+    graph(x, y)
 
 
 init()
